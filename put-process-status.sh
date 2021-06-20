b6a3ece1754562d9459201d7424c90d5ddb00bd3
kensuuu <kensuke.11288@gmail.com>
2021-06-21 08:20:05 +0900

    COMMIT

---
+++ put-process-status.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#######################################
+# check process
+# check memory usage
+#######################################
+PROCESS_NAME=process_name
+
+function is_process_alive() {
+	count = `ps awux | grep -v grep | grep -w $PROCESS_NAME | wc -l`
+	if [[ $count -eq 0 ]]; then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
+#instance config
+readonly INSTANCE_ID='your_instance_id'
+readonly REGION='your_instance_region'
+
+#process count
+PROCESS_COUNT=`is_process_alive $@`
+
+USEDMEMORY=$(free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }')
